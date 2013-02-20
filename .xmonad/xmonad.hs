f08cb7cbf368ceea76072767a1037d6253ab6715
Beto Meijome <beto@freelancer.com>
2013-02-20 21:57:59 +1100

    Multiple Changes
    - Defined sources
    - Added cycle through workspace hooks
    - added own key mappings
    - hook for calling shutter as needed
    - others which i forget now

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,34 +1,63 @@
+-- Sources:
+-- Workspace cycling http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Actions-CycleWS.html
+-- adding keymaps rather than replacing http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Doc-Extending.html#Editing_the_layout_hook
+-- Fading inactive windows http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-FadeInactive.html
+-- Default XmonadConfig http://xmonad.org/xmonad-docs/xmonad/src/XMonad-Config.html (or in this directory for convenience)
+-- Interesting overview http://thinkingeek.com/2011/11/21/simple-guide-configure-xmonad-dzen2-conky/
+
 -- Imports.
 import XMonad
-import XMonad.Layout.HintedGrid
+-- import XMonad.Layout.HintedGrid
+import XMonad.Layout.Grid
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Config.Xfce
 import qualified Data.Map as M
- 
+import XMonad.Actions.CycleWS
+import XMonad.Hooks.FadeInactive
+
 main = xmonad myConfig 
 
--- The main function.
--- main = xmonad =<< statusBar toggleStrutsKey myConfig
-
--- Command to launch the bar.
-myBar = "xmobar"
-
--- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
-
+-- Main configuration, override the defaults to your liking.
+myConfig = xfceConfig { 
+    borderWidth = 2
+   -- , terminal = "urxvt"
+   , terminal = "xfce4-terminal"
+--   , modMask = mod4Mask
+   , manageHook = manageHook defaultConfig
+      <+> composeAll myManagementHooks
+      <+> manageDocks
+   , keys= myKeys <+> keys defaultConfig 
+   , logHook = myLogHook
+   }
 
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+     -- /usr/include/X11/keysymdef.h
     [
      -- Key binding to toggle the gap for the bar.
      ((modMask, xK_b), sendMessage ToggleStruts)
     , ((modMask .|. shiftMask, xK_q), spawn "xfce4-session-logout")
+    , ((modMask,               xK_p     ), spawn "dmenu_run") -- %! Launch dmenu
     -- , ((modMask , xK_s     ), spawn "xflock")   
+    , ((modMask .|. shiftMask, xK_Print), spawn "~/bin/screencap_selection.sh") 
+   -- a basic CycleWS setup
+   , ((modMask .|. controlMask, xK_Down),  nextWS)
+   , ((modMask .|. controlMask, xK_Up),    prevWS)
+   , ((modMask .|. shiftMask, xK_Down),  shiftToNext)
+   , ((modMask .|. shiftMask, xK_Up),    shiftToPrev)
+   , ((modMask,               xK_Right), nextScreen)
+   , ((modMask,               xK_Left),  prevScreen)
+   , ((modMask .|. shiftMask, xK_Right), shiftNextScreen)
+   , ((modMask .|. shiftMask, xK_Left),  shiftPrevScreen)
+   , ((modMask,               xK_z),     toggleWS)
     ]
+
+
+myLogHook :: X ()
+myLogHook = fadeInactiveLogHook fadeAmount
+    where fadeAmount = 0.90
  
-
-
 myManagementHooks :: [ManageHook]
 myManagementHooks = [
   resource =? "synapse" --> doIgnore
@@ -43,14 +72,17 @@ myManagementHooks = [
  -- , (className =? "Gimp-2.8") --> doF (W.shift "9:Pix")
   ]
 
--- Main configuration, override the defaults to your liking.
-myConfig = xfceConfig { 
-    borderWidth = 2
-   -- , terminal = "urxvt"
-   , terminal = "xfce4-terminal"
---   , modMask = mod4Mask
-   , manageHook = manageHook defaultConfig
-      <+> composeAll myManagementHooks
-      <+> manageDocks
-   
-   }
+-- Based on config layout
+layout = tiled ||| Mirror tiled ||| Full ||| GridRatio (4/4)
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 5/100
