012b3272286fe7bd7c366608eea7da1fa1df40b4
Beto Meijome <beto@freelancer.com>
2013-10-22 08:21:53 +1100

    updated to run better via cron

--- bin/check_battery_level.sh
+++ bin/check_battery_level.sh
@@ -1,56 +1,58 @@
 #! /bin/bash
 # Original from https://faq.i3wm.org/question/1730/warning-popup-when-battery-very-low/
 
-SLEEP_TIME=5   # Default time between checks.
-SAFE_PERCENT=30  # Still safe at this level.
-DANGER_PERCENT=15  # Warn when battery at this level.
+SAFE_PERCENT=99  # Still safe at this level.
+DANGER_PERCENT=98  # Warn when battery at this level.
 CRITICAL_PERCENT=5  # Hibernate when battery at this level.
 
 NAGBAR_PID=0
+PID_FILE="/tmp/check_battery_level.pid"
+CMD_HIBERNATE="/usr/sbin/pm-hibernate"
+
 export DISPLAY=:0.0
 
 function launchNagBar
 {
-    i3-nagbar -m 'Battery low!' -b 'Hibernate!' 'pm-hibernate' >/dev/null 2>&1 &
-    NAGBAR_PID=$!
+    i3-nagbar -m "Battery low: ${1}%!" -b 'Hibernate!' "$CMD_HIBERNATE" >/dev/null 2>&1 &
+    NAGBAR_PID=$!; echo $NAGBAR_PID > $PID_FILE
+}
+
+function fyiHibernate 
+{
+    i3-nagbar -m "BATTERY IS TOO LOWi (${1}%) - HIBERNATING NOW!" -b 'Hibernate!' "$CMD_HIBERNATE" >/dev/null 2>&1 &
+    NAGBAR_PID=$!; echo $NAGBAR_PID > $PID_FILE
+    sleep 5 
+    killNagBar
+    $CMD_HIBERNATE 
+
 }
 
 function killNagBar
 {
-    if [[ $NAGBAR_PID -ne 0 ]]; then
-        ps -p $NAGBAR_PID | grep "i3-nagbar"
-        if [[ $? -eq 0 ]]; then
-            kill $NAGBAR_PID
-        fi
-        NAGBAR_PID=0
-    fi
+    kill `cat $PID_FILE` 2>/dev/null
+    rm -f $PID_FILE
 }
 
 
-while [ true ]; do
+killNagBar
 
-    killNagBar
+if [[ -n $(acpitool -b | grep -i discharging) ]]; then
+    rem_bat=$(acpitool -b | grep -Eo "[0-9]+\.[0-9]+%" | cut -d\. -f 1)
 
-    if [[ -n $(acpi -b | grep -i discharging) ]]; then
-        rem_bat=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
-
-        if [[ $rem_bat -gt $SAFE_PERCENT ]]; then
-            SLEEP_TIME=10
-        else
-            SLEEP_TIME=5
-            if [[ $rem_bat -le $DANGER_PERCENT ]]; then
-                SLEEP_TIME=2
-                launchNagBar
-            fi
-            if [[ $rem_bat -le $CRITICAL_PERCENT ]]; then
-                SLEEP_TIME=1
-                pm-hibernate
-            fi
-        fi
-    else
+    if [[ $rem_bat -gt $SAFE_PERCENT ]]; then
         SLEEP_TIME=10
+    else
+        SLEEP_TIME=5
+        killNagBar
+        if [[ $rem_bat -le $DANGER_PERCENT ]]; then
+            SLEEP_TIME=2
+            launchNagBar $rem_bat
+        fi
+        if [[ $rem_bat -le $CRITICAL_PERCENT ]]; then
+            fyiHibernate 
+        fi
     fi
+else
+    SLEEP_TIME=10
+fi
 
-    sleep ${SLEEP_TIME}m
-
-done
