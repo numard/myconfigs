1eba38adabec972c5f4824b5560efc77237593e9
Beto Meijome <beto@freelancer.com>
2014-04-25 15:51:55 +1000

    Removing old API key

--- bin/aws_systest.sh
+++ bin/aws_systest.sh
@@ -5,10 +5,5 @@ export EC2_HOME=~/bin/ec2-api-tools/
 # Beto's own auth info
 export EC2_PRIVATE_KEY=~/_1/access_aws/sys-test/beto-systest.iam.pk.pem
 export EC2_CERT=~/_1/access_aws/sys-test/beto-systest.iam.cert.pem
-
-# for user beto-systest ( iam role)
-export EC2_API_KEY=AKIAJE43AF3L4UXNTDIQ
-export EC2_API_SECRET=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
-
 # ensure this points to aws.crentials-SYSTEST
 export AWS_CREDENTIAL_FILE=~/.aws-credentials-SYSTEST
